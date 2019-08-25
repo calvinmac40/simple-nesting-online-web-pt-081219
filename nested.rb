78bf25eb2c3b4ce830c3d3a0d8f7f9658a9f2f23
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-25 21:19:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,8 +2,7 @@
 def hopper
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
+        :grace_hopper => {:known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
@@ -15,8 +14,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for] = "Object Orientation"     
 end
 
 def dennis_ritchies_language
@@ -55,6 +54,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def adding_matz
@@ -80,7 +80,7 @@ def adding_matz
         }
      }
 
-    
+ programmer_hash[:yukihiro_matsumoto] = :known_for "Ruby" :languages "LISP" "C" 
 end
 
 def changing_alan
